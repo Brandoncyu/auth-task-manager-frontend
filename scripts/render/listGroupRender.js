8f4448f78dd35ded6eeed1d6c0eac650a41bfe33
Wendy Parsons <wsparsons@gmail.com>
2018-07-23 15:30:01 -0700

    added function for creating new task

--- scripts/render/listGroupRender.js
+++ scripts/render/listGroupRender.js
@@ -1,11 +1,40 @@
-const createNewListsTemplate = require('../templates/createNewList')
+const createNewTaskTemplate = require('../templates/createNewTask')
 const cardDoing = require('../templates/cardDoing')
 const cardDone = require('../templates/cardDone')
+
 const moment = require('moment')
 
 function listGroupRender(lists, element) {
-  document.getElementById('task-form').innerHTML = createNewListsTemplate()
+  document.getElementById('task-form').innerHTML = createNewTaskTemplate()
   let dataId = element.getAttribute('data-id')
+
+  let createTaskForm = document.getElementById('create-task-form')
+  createTaskForm.addEventListener('submit', function(event) {
+    event.preventDefault()
+    let taskTitle = document.getElementById('task-title').value
+    let taskContent = document.getElementById('task-content').value
+
+    axios(`${baseURL}/api/lists/${dataId}/tasks`, {
+      method: `POST`,
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      },
+      data: {
+        title: taskTitle,
+        description: taskContent,
+        list_id: dataId
+      }
+    }).then(response => {
+      console.log(response)
+      const allList = require('./allList')
+      document.getElementById('task-title').value = ''
+      document.getElementById('task-content').value = ''
+      allList()
+
+    }).catch(error => {
+      console.log(error)
+    })
+  })
 
   let taskList = lists.find(task => task.id === parseInt(dataId)).tasks
 
